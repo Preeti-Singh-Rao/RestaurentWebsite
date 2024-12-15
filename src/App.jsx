3054df326a8934e2db2a523776e57003cb37c800
Preeti-Singh-Rao <preetirao200116@gmail.com>
2024-12-15 15:13:14 +0530

    add mealssummary

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import { Fragment } from 'react'
 import './App.css'
 import Header from "./Components/Layout/Header"
 
+import MealsSummary from './Components/Layout/Meals/MealsSummary';
+
 function App() {
  
 
   return (
     <Fragment>
       <Header/>
+      <MealsSummary/>
     </Fragment>
   )
 }
---
+++ src/Components/Layout/Meals/MealsSummary.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./MealsSummary.css";
+
+const MealsSummary=()=>{
+    return(
+        <div className="summary">
+            <h2 className="h2">Delicious Food, Delivered To You</h2>
+            <p>Choose your favorite meal from our board 
+            selection of available meals and enjoy a delicious lunch or dinner at home.</p>
+            <p>All our meals are cooked with high quality ingredients,
+                 just-in-time and ofcourse by experienced chefs!</p>
+        </div>
+    )
+
+}
+
+export default MealsSummary;
